eb3d5279d7779028b699038b145924051b15f06c
mathur-exe <gmathur1993@gmail.com>
2022-05-17 12:01:20 +0530

    darknet

--- darknet.py
+++ darknet.py
@@ -38,8 +38,8 @@ def parse_cfg(cfgfile):
   for line in lines:
     if line[0] == '[':                      # '[' this marks the start of a new block
       if len(block) != 0:                   # !block.empty  --> block is storing some values 
-        blocks.append(block)                # 
-        block = {}
+        blocks.append(block)                # add it to blocks list
+        block = {}                          # re-init the block
       block['type'] = line[1:-1]
     else:
       key, value = line.split('=')
@@ -47,4 +47,95 @@ def parse_cfg(cfgfile):
   blocks.append(block)
 
   return blocks
+
+"""
+What's an empty layer?
+- 
+"""
+class EmptyLayer(nn.Module):
+    def __init__(self):
+        super(EmptyLayer, self).__init__()
+
+def create_modules(blocks):
+  net_info = blocks[0]                # capturing info about i/p & preprocessing
+  module_list = nn.ModuleList()
+  prev_filter = 3                   
+  # used to track the no. of filter in the layer which the convolution layer is appled
+  # has been initalized to 3 filters corresponding to RGB channels
+  output_filters = []     # stores the number of o/p filters 
+
+  # iterate over the list of blocks, and create a PyTorch module for each block
+  for index, x in enumerate(blocks[1:]):
+    module = nn.Sequential()
+    
+    """
+    Convolution Layer
+    - 
+    """ 
+    if (x["type"] == "convolutional"):
+      #Get the info about the layer
+      activation = x["activation"]
+      try:
+        batch_normalize = int(x["batch_normalize"])
+        bias = False
+      except:
+        batch_normalize = 0
+        bias = True
+        
+      filters= int(x["filters"])
+      padding = int(x["pad"])
+      kernel_size = int(x["size"])
+      stride = int(x["stride"])
+        
+      if padding:
+        pad = (kernel_size - 1) // 2
+      else:
+        pad = 0
+        
+      #Add the convolutional layer
+      conv = nn.Conv2d(prev_filters, filters, kernel_size, stride, pad, bias = bias)
+      module.add_module("conv_{0}".format(index), conv)
+        
+      #Add the Batch Norm Layer
+      if batch_normalize:
+        bn = nn.BatchNorm2d(filters)
+        module.add_module("batch_norm_{0}".format(index), bn)
+        
+      #Check the activation. 
+      #It is either Linear or a Leaky ReLU for YOLO
+      if activation == "leaky":
+        activn = nn.LeakyReLU(0.1, inplace = True)
+        module.add_module("leaky_{0}".format(index), activn)
+    
+
+    """
+    Route Layer / Shortcut Layer
+    - Extract the values of the layer attribute 
+    - Cat ti into an int and store in list
+    """
+    elif (x["type"] == "route"):
+      x["layers"] = x["layers"].split(',')
+      #Start  of a route
+      start = int(x["layers"][0])
+      #end, if there exists one.
+      try:
+        end = int(x["layers"][1])
+      except:
+        end = 0
+      #Positive anotation
+      if start > 0: 
+        start = start - index
+      if end > 0:
+        end = end - index
+      route = EmptyLayer()
+      module.add_module("route_{0}".format(index), route)
+      if end < 0:
+        filters = output_filters[index + start] + output_filters[index + end]
+      else:
+        filters= output_filters[index + start]
+
+      #shortcut corresponds to skip connection
+      elif x["type"] == "shortcut":
+        shortcut = EmptyLayer()
+        module.add_module("shortcut_{}".format(index), shortcut)
 

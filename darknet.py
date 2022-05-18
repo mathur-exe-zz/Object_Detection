0b32ad2312d0b71b1edbde7511d6625f100f62d2
mathur-exe <gmathur1993@gmail.com>
2022-05-18 16:16:14 +0530

    -

--- darknet.py
+++ darknet.py
@@ -171,3 +171,53 @@ def create_modules(blocks):
 blocks = parse_cfg('/content/yolov3.cfg')
 print(create_modules(blocks))
 
+class Darknet(nn.Module):
+  def __inint__(self, cfgfile):
+    super(Darknet, self).__init__()
+    self.blocks = parse_cfg(cfgfile)
+    self.net_info, self.module_list = create_modules(self.blocks)
+
+  # Purpose of forward() is to 
+  # 1. Calculate ouput
+  # 2. Transform o/p detection map in a way that it can be preprocesses 
+  def forward(self, x, CUDA):
+    modules = self.block[1:]      
+    # self.blocks[1:] is used instead of self.block since 1st element is a net block and isn't a part of forward pass
+    outputs = {}
+    # route & shortcut layers need output may from prev_layers, we cache the output feature map of every layer 
+
+    write = 0
+    for i, module in enumerate(modules):
+      module_type = (module['type'])
+
+      if module_type == 'convolutional' or module_type == 'upsample':
+        x = self.module_list[i](x)
+
+      elif module_type == 'route':
+        """
+        Here we have to acct for 2 cases - Route & shortcut
+        - thus we use torch.cat to concatenate 2 feature maps 
+          (w/ 2nd argument at pos_1 as we are concatenating along the depth)
+        """
+        layers = module["layers"]
+        layers = [int(a) for a in layers]
+
+        if (layers[0]) > 0:
+          layers[0] = layers[0] - i
+
+        if len(layers) == 1:
+          x = outputs[i + (layers[0])]
+
+        else:
+          if (layers[1]) > 0:
+            layers[1] = layers[1] - i
+
+            map1 = outputs[i + layers[0]]
+            map2 = outputs[i + layers[1]]
+
+            x = torch.cat((map1, map2), 1)
+
+      elif  module_type == "shortcut":
+        from_ = int(module["from"])
+        x = outputs[i-1] + outputs[i+from_]
+
